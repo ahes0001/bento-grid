31d3b5a88aa4b4d080af032148a9c818dc023ecc
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-12-08 09:45:17 +0300

    adjusted for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ function App() {
         <h1 className="text-white text-center">
           Social Media <span className="text-yellow500">10x</span> Faster with AI
         </h1>
-        <img className = "my-[5%] w-[200px]" src="\images\illustration-five-stars.webp" alt="" />
+        <img className = "my-[5%] w-[200px]" src="/images/illustration-five-stars.webp" alt="" />
         <h4 className="text-white">
           Over 4,000 5-star reviews
         </h4>
       </div>
 
       <div className="bg-white rounded-lg lg:col-start-2 lg:col-end-3 lg:row-start-11 lg:row-end-18 p-[5%] flex flex-col justify-center items-center justify- ">
-      <img src="\images\illustration-multiple-platforms.webp" alt="" />
+      <img src="/images/illustration-multiple-platforms.webp" alt="" />
       <h3>
       Manage multiple accounts and platforms.
       </h3>
@@ -32,14 +32,14 @@ function App() {
         <h3 className="mb-[5%]">
           Schedule to social media.
         </h3> 
-        <img className="relative lg:inset-x-7 sm:h-[75%] lg:my-[5%] lg:h-[30vh] h-fit w-fit" src="\images\illustration-schedule-posts.webp" alt="" />
+        <img className="relative lg:inset-x-7 sm:h-[75%] lg:my-[5%] lg:h-[30vh] h-fit w-fit" src="/images/illustration-schedule-posts.webp" alt="" />
         <h4 className="mt-[5%] px-4 lg:text-left">
           Optimize post timings to publish content at
           the perfect time for your audience.
         </h4>
       </div>
       <div className="bg-purple500 rounded-lg sm:col-span-2 lg:row-span-7 lg: p-[5%] flex flex-col justify-around items-center text-center sm:flex-row ">
-      <img className=" max-h-[20vh]" src="\images\illustration-grow-followers.webp" alt="" />
+      <img className=" max-h-[20vh]" src="/images/illustration-grow-followers.webp" alt="" />
       <h2 className="object-contain max-w-80">
         Grow followers with non-stop content.
       </h2>
@@ -58,7 +58,7 @@ function App() {
           Create and schedule content <span className="text-purple500 italic">quicker</span>.
         </h3>
         
-        <img className="w-[150px]" src="\images\illustration-create-post.webp" alt="" />
+        <img className="w-[150px]" src="/images/illustration-create-post.webp" alt="" />
       </div>
 
       <div className="bg-yellow500 rounded-lg  p-[5%] lg:col-start-1 lg:col-end-2 lg:row-start-13 lg:row-end-25 flex flex-col justify-around">
@@ -66,7 +66,7 @@ function App() {
           Write your content using AI.
         </h3>
         
-        <img className="w-fit h-fit object-contain overflow-hidden" src="\images\illustration-ai-content.webp" alt="" />
+        <img className="w-fit h-fit object-contain overflow-hidden" src="/images/illustration-ai-content.webp" alt="" />
       </div>
     </div>
   );
