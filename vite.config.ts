9bf32666d4b83978350abac8ac1f4a4d2565ec52
Ahmed Khadawardi <ahes0001@student.monash.edu>
2024-12-08 09:07:27 +0300

    edited scrips for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: '/bento-grid/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
